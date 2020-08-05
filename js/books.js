cc7bbd858d6e3f5f362ee3a33f53ecb97b31edbb
Michael-Mbaya <gikundamike@gmail.com>
2020-08-05 12:03:54 +0300

    Small description changed to tooltip

---
+++ js/books.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+// $(document).ready(function(){
+//     $("#cravenHover").hover(function(){
+//       $("#cravenText").toggle();
+//     }, function(){
+//     $("#cravenText").toggle();
+//     });
+//   });
+
+// $(document).ready(function(){
+//     $("#liesHover").hover(function(){
+//       $("#liesText").toggle();
+//     }, function(){
+//     $("#liesText").toggle();
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("#deepHover").hover(function(){
+//       $("#deepText").toggle();
+//     }, function(){
+//     $("#deepText").toggle();
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("#shiningHover").hover(function(){
+//       $("#shiningText").toggle();
+//     }, function(){
+//     $("#shiningText").toggle();
+//     });
+//   });
